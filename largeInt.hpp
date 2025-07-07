f22b99cda679456e3861fc9f8bbd4e5357d8dcbe
Marek Oczadly <moczadly@outlook.com>
2025-07-07 14:23:13 +0100

    Created UInt2048

---
+++ largeInt.hpp
@@ -0,0 +1,14 @@
+// Author: Marek Oczadly
+
+
+#pragma once
+#include <array>
+#include <intrin.h>		// For SIMD instructions
+
+
+class UInt2048 {
+private:
+	std::array<unsigned long long, 32> data;
+public:
+
+};
